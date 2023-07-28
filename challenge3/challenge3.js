0204e8392ef0afe1fa2b3a5d3dc106c465cf1e4d
Samuel <samuguemar@gmail.com>
2023-07-28 20:26:01 +0100

    fix: challenge 3 to pass the new test

--- challenge3/challenge3.js
+++ challenge3/challenge3.js
@@ -1,5 +1,7 @@
 const OPEN_PARENTHESES = '('
 const CLOSE_PARENTHESES = ')'
+const OPEN_BRACKET = '['
+const OPEN_BRACE = '{'
 
 export const isValid = (letter) => {
   let parenthesesCount = 0
@@ -7,6 +9,8 @@ export const isValid = (letter) => {
 
   for (let i = 0; i < letter.length; i++) {
     currentChar = letter[i]
+    if (currentChar === OPEN_BRACE) return false
+    if (currentChar === OPEN_BRACKET) return false
     if (currentChar === OPEN_PARENTHESES) parenthesesCount++
     if (currentChar === CLOSE_PARENTHESES) parenthesesCount--
     if (parenthesesCount < 0) return false
