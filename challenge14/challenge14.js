4682eb60e272e824dd811ac121dc1ed1e85126b7
Samuel <samuguemar@gmail.com>
2023-08-18 19:31:23 +0100

    refactor: challenge 14 with binary search

--- challenge14/challenge14.js
+++ challenge14/challenge14.js
@@ -1,8 +1,16 @@
 export default function missingReindeer (ids) {
+  if (ids.length === 1) return 1 - ids[0]
   ids.sort((a, b) => a - b)
-  const maxId = ids[ids.length - 1]
-  for (let i = 0; i <= maxId; i++) {
-    if (i !== ids[i]) return i
+
+  let missingIds = [...ids]
+  while (missingIds.length > 2) {
+    const half = Math.floor(missingIds.length / 2)
+    missingIds[half] - missingIds[0] === half
+      ? missingIds = missingIds.slice(half)
+      : missingIds = missingIds.slice(0, half)
   }
-  return maxId + 1
+
+  const maxId = missingIds[1]
+  const minId = missingIds[0]
+  return maxId - minId === 1 ? maxId + 1 : minId + 1
 }
