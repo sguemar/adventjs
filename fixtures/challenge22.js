89cc9a68fc3dbd7892902605028920f5247d9331
Samuel <samuguemar@gmail.com>
2023-08-25 19:52:18 +0100

    test: add challenge 22 tests

---
+++ fixtures/challenge22.js
@@ -0,0 +1,115 @@
+export const threeLevelTreeWith17Decorations = {
+  value: 1,
+  left: {
+    value: 2,
+    left: {
+      value: 3,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 3,
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    value: 2,
+    left: {
+      value: 3,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 3,
+      left: null,
+      right: null
+    }
+  }
+}
+
+export const threeLevelTreeWith40Decorations = {
+  value: 1,
+  left: {
+    value: 4,
+    left: {
+      value: 10,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 5,
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    value: 6,
+    left: {
+      value: 1,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 13,
+      left: null,
+      right: null
+    }
+  }
+}
+
+export const asymmetricTreeWith28Decorations = {
+  value: 1,
+  left: {
+    value: 5,
+    left: {
+      value: 7,
+      left: {
+        value: 3,
+        left: null,
+        right: null
+      },
+      right: null
+    },
+    right: null
+  },
+  right: {
+    value: 6,
+    left: {
+      value: 5,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 1,
+      left: null,
+      right: null
+    }
+  }
+}
+
+export const asymmetricTreeWith17Decorations = {
+  value: 1,
+  left: null,
+  right: {
+    value: 2,
+    left: {
+      value: 3,
+      left: {
+        value: 4,
+        left: null,
+        right: null
+      },
+      right: null
+    },
+    right: {
+      value: 3,
+      left: null,
+      right: {
+        value: 4,
+        left: null,
+        right: null
+      }
+    }
+  }
+}
