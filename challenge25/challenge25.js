b894131e432765e6b548b7f9ee5d20763ac8e24c
Samuel <samuguemar@gmail.com>
2023-09-18 19:39:22 +0100

    refactor: change the if conditions for an object

--- challenge25/challenge25.js
+++ challenge25/challenge25.js
@@ -1,20 +1,18 @@
 export default function canMouseEat (direction, room) {
+  const MOUSE = 'm'
+  const FOOD = '*'
+
+  const MOVES = {
+    right: (i, j) => room[i][j + 1],
+    left: (i, j) => room[i][j - 1],
+    up: (i, j) => room[i - 1]?.[j],
+    down: (i, j) => room[i + 1]?.[j]
+  }
+
   for (let i = 0; i < room.length; i++) {
     for (let j = 0; j < room[i].length; j++) {
-      if (room[i][j] !== 'm') continue
-      if (direction === 'right') {
-        if (room[i][j + 1] === '*') return true
-      }
-      if (direction === 'left') {
-        if (room[i][j - 1] === '*') return true
-      }
-      if (direction === 'up') {
-        if (room[i - 1]?.[j] === '*') return true
-      }
-      if (direction === 'down') {
-        if (room[i + 1]?.[j] === '*') return true
-      }
+      if (room[i][j] !== MOUSE) continue
+      return MOVES[direction](i, j) === FOOD
     }
   }
-  return false
 }
