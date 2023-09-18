d82b4b83053055010a57754f7be0f7f6e7f694c8
Samuel <samuguemar@gmail.com>
2023-09-18 19:27:36 +0100

    refactor: remove duplicated conditions

--- challenge25/challenge25.js
+++ challenge25/challenge25.js
@@ -1,17 +1,18 @@
 export default function canMouseEat (direction, room) {
   for (let i = 0; i < room.length; i++) {
     for (let j = 0; j < room[i].length; j++) {
+      if (room[i][j] !== 'm') continue
       if (direction === 'right') {
-        if (room[i][j] === 'm' && room[i][j + 1] === '*') return true
+        if (room[i][j + 1] === '*') return true
       }
       if (direction === 'left') {
-        if (room[i][j] === 'm' && room[i][j - 1] === '*') return true
+        if (room[i][j - 1] === '*') return true
       }
       if (direction === 'up') {
-        if (room[i][j] === 'm' && room[i - 1]?.[j] === '*') return true
+        if (room[i - 1]?.[j] === '*') return true
       }
       if (direction === 'down') {
-        if (room[i][j] === 'm' && room[i + 1]?.[j] === '*') return true
+        if (room[i + 1]?.[j] === '*') return true
       }
     }
   }
